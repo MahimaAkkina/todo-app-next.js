4542e9651ba7b86b44aa8a994ade00179e096246
MahimaAkkina <mahimaakkina@gmail.com>
2025-12-02 16:27:48 +0530

    Added bootstrap cdn link in head section in htmllayout function to apply styles to Todo main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function HtmlLayout({
   }>) {
     return(
       <html lang="en">
+        <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        </head>
         <body>
           {children}
         </body>
