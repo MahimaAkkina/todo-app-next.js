d801bf7024ab032cfa212526960011a83f7ad463
MahimaAkkina <mahimaakkina@gmail.com>
2025-12-04 16:05:47 +0530

    Changes in description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function HtmlLayout({
     return(
       <html lang="en">
         <head>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
         </head>
         <body>
           {children}
