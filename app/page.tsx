87be453f8235530dd4d79675f16e0ebe32f2f27d
MahimaAkkina <mahimaakkina@gmail.com>
2025-12-07 22:07:19 +0530

    Added text-decoration-line-through property for tasks by adding complete button

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,22 @@
 import "./globals.css";
 import {useState} from "react";
 export default function TodoPage(){
-  const [inputTask,setInputTask]=useState("");
-  const [tasks,setTasks]=useState<string[]>([]);
+  const [inputTask,setInputTask]=useState<string>("");
+  type Task={text: string;completed: boolean;};
+  const [tasks,setTasks]=useState<Task[]>([]);
   const addTask=()=>{
     if(inputTask.trim()===""){
       return;
     }
-    setTasks([...tasks,inputTask]); //creating new array by adding new task ast end
+    // setTasks([...tasks,inputTask]); //creating new array by adding new task ast end
+    setTasks([...tasks,{text:inputTask, completed:false}]);
     setInputTask(""); //clears the input box
 
   };
-  // const completeTask=(idToComp:number)=>{
-  //   setTasks(task)
-  // }
+  const completeTask=(idToComp:number)=>{
+    setTasks(tasks.map((task,index)=> index===idToComp ?{...task,completed:!task.completed} : task
+    ))
+  }
   const delTask=(idToDel:number)=>{
     setTasks(tasks.filter((_,index)=>index!==idToDel));
 
@@ -30,9 +33,12 @@ export default function TodoPage(){
       </div>
       <ul className="list-group mt-4">
         {tasks.map((task,index)=>(
-        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{task}
-        <button className="btn btn-success btn-sm" onClick={()=>completeTask(index)}>Complete</button>
-        <button className="btn btn-danger btn-sm" onClick={()=>delTask(index)}>Delete</button>
+        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+          <span className={task.completed ? "text-decoration-line-through" : ""}>{task.text} </span>
+        <div className="d-flex gap-2">
+          <button className="btn btn-success btn-sm" onClick={()=>completeTask(index)}>Complete</button>
+          <button className="btn btn-danger btn-sm" onClick={()=>delTask(index)}>Delete</button>
+        </div>
         </li>
         ))}
         {/* <li className="list-group-item d-flex justify-content-between align-items-center">Sample Task
