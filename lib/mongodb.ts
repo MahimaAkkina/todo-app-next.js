3da558a21bb7991e7b20a44abde62167ad43187a
MahimaAkkina <mahimaakkina@gmail.com>
2025-12-12 03:46:26 +0530

    Created connectdb function to establish MongoDB connection Added ready state check also to prevent multiple connections

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,8 @@
 //connectiong our app to mongodb
 
 import mongoose from "mongoose";
-const mongodb_url="";
-export const connect=asynce()=>{
+const mongodb_url="mongodb+srv://mahimaakkina_db_user:GodisLove%40143@cluster0.l6kxe98.mongodb.net/TodoTask";
+export const connectdb=async()=>{
     if(mongoose.connection.readyState>=1){
         return;
     }
